998a81139ec8a0fb88e07b2d5bedf25d34281c6a
Kenny Baron <kennybaron@fastmail.com>
2023-10-28 15:25:58 -0400

    cleanup system.rs

--- src/systems.rs
+++ src/systems.rs
@@ -98,26 +98,6 @@ fn set_rotatablebody_component(
     // todo update heading
 }
 
-// fn process_player_control_keys(&mut self) {
-// let mut query = <(&HumanInputCpt, &mut RotationalInputCpt)>::query();
-
-// let input = &self.input;
-// let runstate = self.get_runstate();
-
-// for (_human, mut rotational_input) in query.iter_mut(&mut self.world) {
-//     set_rotational_input(input, runstate, &mut rotational_input);
-// }
-
-// if self.input.key_pressed(VirtualKeyCode::Space)
-//     || self.input.key_held(VirtualKeyCode::Space)
-// {
-//     let mut query = <&mut CraftActionStateCpt>::query();
-//     for state in query.iter_mut(&mut self.world) {
-//         state.is_firing_primary = true;
-//     }
-// }
-// }
-
 // todo Ideally: key input event -> key<->control mapping -> control event or set control component
 fn set_rotational_input_component_by_human(
     input: &WinitInputHelper,
@@ -146,6 +126,7 @@ fn set_rotational_input_component_by_human(
 ////////////////////////////////////////////////////////////////////////////////
 // Projectile Creation
 ////////////////////////////////////////////////////////////////////////////////
+
 pub fn system_projectile_emission(world: &mut World) {
     let mut projectiles_to_spawn: Vec<ArchProjectile> = vec![];
     for (ent, (tx, pe, cc)) in
@@ -175,6 +156,7 @@ pub fn system_projectile_emission(world: &mut World) {
 ////////////////////////////////////////////////////////////////////////////////
 // Integrate for Motion
 ////////////////////////////////////////////////////////////////////////////////
+
 pub fn system_integrate_rotation(world: &mut World, dt: &Dt) {
     for (id, (transform, rotatablebody)) in
         world.query_mut::<(&mut TransformCpt, &RotatableBodyCpt)>()
@@ -193,6 +175,7 @@ pub fn system_integrate_translation(world: &mut World, dt: &Dt) {
 ////////////////////////////////////////////////////////////////////////////////
 // React to Game World Boundary
 ////////////////////////////////////////////////////////////////////////////////
+
 pub fn system_boundary_restrict_circloid(world: &mut World) {
     for (id, (transform, rigidbody, collision_circle)) in
         world.query_mut::<(&mut TransformCpt, &mut RigidBodyCpt, &CircleColliderCpt)>()
@@ -276,6 +259,7 @@ pub fn test_system_boundary_restrict_particle(world: &mut World) {
 ////////////////////////////////////////////////////////////////////////////////
 // Collision Detection
 ////////////////////////////////////////////////////////////////////////////////
+
 pub fn system_collision_detection(world: &mut World) {
     let mut colliding_entities: Vec<(Entity, Entity)> = vec![];
     let circloid_components: Vec<(Entity, TransformCpt, CircleColliderCpt)>;
@@ -333,6 +317,7 @@ pub fn system_collision_detection(world: &mut World) {
 ////////////////////////////////////////////////////////////////////////////////
 // Collision Resolution Dispatcher
 ////////////////////////////////////////////////////////////////////////////////
+
 pub fn system_collision_resolution(world: &mut World) {
     // Different resolutions depending on the kind of collision detection event
     // e.g. Dispatches more event components to be handled by downstream systems
@@ -376,120 +361,3 @@ pub fn system_collision_resolution(world: &mut World) {
         world.despawn(pair.1);
     }
 }
-
-// #[system]
-// pub fn circle_collision(
-//     query: &mut Query<(Entity, &TransformCpt, &CircleColliderCpt)>,
-//     commands: &mut CommandBuffer,
-//     world: &mut SubWorld,
-// ) {
-//     let mut entities: Vec<(&Entity, &TransformCpt, &CircleColliderCpt)> = vec![];
-//     let chunks = query;
-//     for (entity_a, tx, cx) in chunks.iter(world) {
-//         entities.push((entity_a, tx, cx));
-//     }
-//     let mut colliding_entities: Vec<&Entity> = vec![];
-//     for (i, (entity1, tx1, cx1)) in entities.iter().enumerate() {
-//         for (entity2, tx2, cx2) in entities[i + 1..].iter() {
-//             let dx = tx2.position.x - tx1.position.x;
-//             let dy = tx2.position.y - tx1.position.y;
-//             let dr = (dx.powf(2.0) + dy.powf(2.0)).sqrt();
-//             if dr < (cx1.r + cx2.r) {
-//                 dev!("COLLISION DETECTED");
-//                 colliding_entities.push(entity1);
-//                 colliding_entities.push(entity2);
-//             }
-//         }
-//     }
-//     for &entity in colliding_entities {
-//         commands.remove(entity);
-//     }
-
-// for (i, entity1, transform, collision_circle) in query.iter(world).enumerate() {
-//     for (entity2, transform, collision_circle) in )
-
-// }
-// }
-
-// #[system(for_each)]
-// pub fn process_translational_input(
-//     input: &TranslationalInputCpt,
-//     movestats: &MovementStatsCpt,
-//     rigidbody: &mut RigidBodyCpt,
-// ) {
-//     match input.direction {
-//         Some(Direction::E) => {
-//             rigidbody.velocity.x = movestats.speed as f32;
-//             rigidbody.velocity.y = 0f32;
-//         }
-//         Some(Direction::SE) => {
-//             let component_speed_f32 = movestats.speed as f32;
-//             let component_speed = (component_speed_f32.powi(2) / 2.0).sqrt();
-//             rigidbody.velocity.x = component_speed;
-//             rigidbody.velocity.y = component_speed;
-//         }
-//         Some(Direction::S) => {
-//             rigidbody.velocity.x = 0f32;
-//             rigidbody.velocity.y = movestats.speed as f32;
-//         }
-//         Some(Direction::SW) => {
-//             let component_speed_f32 = movestats.speed as f32;
-//             let component_speed = (component_speed_f32.powi(2) / 2.0).sqrt();
-//             rigidbody.velocity.x = -component_speed;
-//             rigidbody.velocity.y = component_speed;
-//         }
-//         Some(Direction::W) => {
-//             rigidbody.velocity.x = -(movestats.speed as f32);
-//             rigidbody.velocity.y = 0f32;
-//         }
-//         Some(Direction::NW) => {
-//             let component_speed_f32 = movestats.speed as f32;
-//             let component_speed = (component_speed_f32.powi(2) / 2.0).sqrt();
-//             rigidbody.velocity.x = -component_speed;
-//             rigidbody.velocity.y = -component_speed;
-//         }
-//         Some(Direction::N) => {
-//             rigidbody.velocity.x = 0f32;
-//             rigidbody.velocity.y = -(movestats.speed as f32);
-//         }
-//         Some(Direction::NE) => {
-//             let component_speed_f32 = movestats.speed as f32;
-//             let component_speed = (component_speed_f32.powi(2) / 2.0).sqrt();
-//             rigidbody.velocity.x = component_speed;
-//             rigidbody.velocity.y = -component_speed;
-//         }
-//         None => {
-//             rigidbody.velocity = Vec2::new(0.0, 0.0);
-//         }
-//     }
-// }
-
-// #[system(for_each)]
-// pub fn world_boundary_bounce_rect(
-//     transform: &mut TransformCpt,
-//     rigidbody: &mut RigidBodyCpt,
-//     collision_area: &BoxColliderCpt,
-//     #[resource] window_dims: &WindowDims,
-// ) {
-//     if transform.position.x + collision_area.w as f32 >= window_dims.w as f32
-//         || transform.position.x < 0f32
-//     {
-//         rigidbody.velocity.x = -rigidbody.velocity.x;
-//     }
-//     if transform.position.x < 0f32 {
-//         transform.position.x = 0f32;
-//     } else if transform.position.x + collision_area.w as f32 >= window_dims.w as f32 {
-//         transform.position.x = (window_dims.w - collision_area.w) as f32;
-//     }
-
-//     if transform.position.y + collision_area.h as f32 >= window_dims.h as f32
-//         || transform.position.y < 0f32
-//     {
-//         rigidbody.velocity.y = -rigidbody.velocity.y;
-//     }
-//     if transform.position.y < 0f32 {
-//         transform.position.y = 0f32;
-//     } else if transform.position.y + collision_area.h as f32 >= window_dims.h as f32 {
-//         transform.position.y = (window_dims.h - collision_area.h) as f32;
-//     }
-// }
